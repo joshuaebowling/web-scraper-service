10efc9d3f6d0ed85e68c3d1c42c54962fc2ed2ee
Josh Bowling <joshuaebowling@gmail.com>
2017-11-28 04:12:16 +0000

    moved argument check rejection to outer-most function

--- scraper.js
+++ scraper.js
@@ -12,15 +12,13 @@ _scrape.parseUrl = function(url) {
 _scrape.parseResponse = function(response) {
   return new Promise((resolve, reject) => {
     resolve(response.html);
-  });  
+  });
 };
 
-_scrape.scrape = function(_url = "", instructions = null) {
+_scrape.scrape = function(_url = "") {
   var response, url;
   url = _scrape.parseUrl(_url);
   return new Promise((resolve, reject) => {
-    if(_url === "" || instructions === null)
-      return reject("Please Provide a URL and a valid instruction set");
     request.get(url, (err, response, body) => {
       if(err) return reject(err);
       resolve(response);
@@ -32,9 +30,11 @@ module.exports = {
   _parseUrl: _scrape.parseUrl,
   _parseResponse: _scrape.parseResponse,
   _scrape: _scrape.scrape,
-  scrape: function(url = "") {
+  scrape: function(url = "", instructions = null) {
     return new Promise((resolve, reject) => {
-      _scrape.scrape(url)
+      if(_url === "" || instructions === null)
+        return reject("Please Provide a URL and a valid instruction set");      
+      _scrape.scrape(url, instructions)
         .then(response => {
           _scrape.parseResponse(response)
             .then(result => resolve(response));
